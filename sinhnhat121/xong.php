2e991b71afe0797a35b4b6e52813b5064c853db3
ÐứÇ <54202075+mrduc0616@users.noreply.github.com>
2019-10-26 20:31:19 +0700

    Add files via upload

---
+++ sinhnhat121/xong.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Happy Birthday Trần Thị Mai 17/07/2012</title>
+<style type="text/css">
+body{
+	background:#000;
+	font-family:Verdana, Geneva, sans-serif;
+	font-size:14px;
+	color:#fff;
+}
+#wrapper{
+	margin:0 auto;
+	margin-top:80px;
+	width:890px;
+}
+#col1{
+	float:left;
+	width:500px;
+}
+#col2{
+	float:left;
+	width:380px;
+}
+h1{
+	font-size:18px;
+	margin:5px 0px;
+	
+}
+.date, .title{
+	text-align:center;
+}
+.date{
+	color:#ccc;
+	font-size:11px;
+}
+#content p{
+	
+}
+#content{
+	color:#dcdcdc;
+	font-size:11px;
+	margin-top:25px;
+}
+#nav li{
+	display:inline;
+	margin-right:15px;
+}
+#nav ul{
+	margin:inherit 0;
+	display:block;
+	text-align:center;
+}
+li a{
+	color:#e68787;
+	text-decoration:none;
+}
+.snhg {
+	height:43px;
+	width:114px;
+	text-align:center;
+	font-size:15px;
+	color:#FFFFFF;
+	font-weight:bold;
+	cursor:pointer;
+	border:none;
+	padding-bottom: 6px;
+}
+</style>
+</head>
+<SPAN style="top: 0px; left: 11px;position: absolute; width: 800px; height: 600px; z-index:1"> <EMBED pluginspage=http://www.macromedia.com/go/getflashplayer src="http://imgfree.21cn.com/free/flash/7.swf" width=800 height=600 type=application/x-shockwave-flash wmode="transparent" menu="false" ;; quality="high"></EMBED></SPAN>
+
+<body>
+<div id="wrapper">
+	<div id="col1">
+    	<img src="http://www.look.yeah1.com/albums/userpics/1125901/244714s51aejc6ya.jpg">
+    </div><!--/#col1-->
+    
+    <div id="col2">
+    	<div class="title"><h1>Chúc Mừng Sinh Nhật Trần Thị Mai</h1></div>
+        <div class="date">17/07/1993 -> 17/07/2012</div>
+        <div class="date">Bước 3/3 (<b>Ước nguyện</b>)</div>
+       
+        <div id="content">
+
+           <img src="cil/qua.gif"</img>.<img src="cil/qua.gif"</img>.<img src="cil/qua.gif"</img>.<img src="cil/qua.gif"</img>.<img src="cil/qua.gif"</img>
+    <br><br><br>
+<center>
+
+<?php
+$ip = $_POST['ip'];
+$httpref = $_POST['httpref'];
+$httpagent = $_POST['httpagent'];
+
+
+$uoc = $_POST['uoc'];
+$todayis = date("l, F j, Y, g:i a") ;
+
+
+$subject = DieuUoc;
+
+
+
+$message = " $todayis [GMT] \n
+
+Tools: $httpagent \n
+IP: $ip \n
+Dieu Uoc: $uoc \n
+";
+
+
+$from = "From: $uocnguyen@timlanh.net\r\n";
+
+mail("timlanh.net@gmail.com", $subject, $message, $from);
+
+?>
+Điều ước của Em là:
+<br>
+<b><font color=#FFFFFF><?php echo $uoc ?></b></font>
+<br>
+Cường mong rằng điều ước của Em sẽ nhanh chóng trở thành sự thật
+<br><br>
+<a href=="Http://timlanh.net"><input type="submit" class="snhg" value="Trang Chủ" style="background:url(cil/bt.png) no-repeat;"></a>
+</center>
+           
+        </div>
+    </div><!--/#col1-->
+   
+
+
+</div><!--/#wrapper-->
+<meta http-equiv="refresh" content="200; URL=http://timlanh.net">
+
+
+</body></html>
