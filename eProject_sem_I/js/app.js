0974bb28527f4dc7f9305c1958c421cb8802debb
ÐứÇ <54202075+mrduc0616@users.noreply.github.com>
2020-05-25 17:08:55 +0700

    Add files via upload

--- eProject_sem_I/js/app.js
+++ eProject_sem_I/js/app.js
@@ -3,50 +3,57 @@ myApp.config(function ($routeProvider) {
 	$routeProvider
 	.when('/', {
 		templateUrl: 'home.html',
-		controller: 'HomeCtrl'
+		controller: 'AppCtrl'
 	})
 	.when('/about', {
 		templateUrl: '',
-		controller: 'HomeCtrl'
+		// controller: 'HomeCtrl'
 	})
 	.when('/contact', {
-		templateUrl: '',
-		controller: 'HomeCtrl'
-	})
-	.when('/signUp', {
-		templateUrl: '',
-		controller: 'HomeCtrl'
+		templateUrl: 'Contacts.html',
+		controller: 'ConCtrl'
 	})
 	.when('/service', {
 		templateUrl: '',
-		controller: 'HomeCtrl'
+		// controller: 'HomeCtrl'
 	})
 	
 });
-myApp.controller('HomeCtrl',function ($scope) {
-	$scope.demo;
-});
-myApp.controller('AppCtrl',function ($scope,$http) {
-	$http.get('data/listApp.json').then(function(listMenu){
-		var leftApp = [];
-		var centerAppMain = [];
-		var rightApp = [];
-		for(var key in listMenu.data){
-			for(var key2 in listMenu.data[key]){
-				if(key == 0){
-					leftApp.push(listMenu.data[key][key2]);
-				}
-				else if(key == 1) {
-					centerAppMain.push(listMenu.data[key][key2]);
-				}
-				else{
-					rightApp.push(listMenu.data[key][key2]);
-				}
-			}
-		}
-		$scope.leftApp = leftApp;
-		$scope.rightApp = rightApp;
-		$scope.centerAppMain = centerAppMain;
-		// console.log(centerAppMain);
+myApp.controller('AppCtrl', function ($scope,$http) {
+	$http.get('data/founder.json').then(function(list){
+		$scope.founders = list.data;
+		// console.log($scope.founders);
 	});
-})
+});
+myApp.controller('ConCtrl', function ($scope,$http) {
+	$http.get('data/news.json').then(function(list){
+		$scope.news = list.data;
+	});
+});
+// myApp.controller('HomeCtrl',function ($scope) {
+// 	$scope.demo;
+// });
+// myApp.controller('AppCtrl',function ($scope,$http) {
+// 	$http.get('data/listApp.json').then(function(listMenu){
+// 		var leftApp = [];
+// 		var centerAppMain = [];
+// 		var rightApp = [];
+// 		for(var key in listMenu.data){
+// 			for(var key2 in listMenu.data[key]){
+// 				if(key == 0){
+// 					leftApp.push(listMenu.data[key][key2]);
+// 				}
+// 				else if(key == 1) {
+// 					centerAppMain.push(listMenu.data[key][key2]);
+// 				}
+// 				else{
+// 					rightApp.push(listMenu.data[key][key2]);
+// 				}
+// 			}
+// 		}
+// 		$scope.leftApp = leftApp;
+// 		$scope.rightApp = rightApp;
+// 		$scope.centerAppMain = centerAppMain;
+// 		// console.log(centerAppMain);
+// 	});
+// })
